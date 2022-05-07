0b1f08e11afe0c3783a7d5d36433fe8450bdcaec
mashenchen <704951265@qq.com>
2022-05-07 14:29:23 +0800

    1.0
    233

---
+++ Assets/scripts/Characters/PlayerController.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator anim;
+    private GameObject attackTarget;
+    private CharacterStats characterStats;
+    private float lastAttackTime;
+    private bool isDead;
+    private float stopDistance;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        characterStats = GetComponent<CharacterStats>();
+        stopDistance = agent.stoppingDistance;
+    }
+    private void OnEnable()
+    {
+        Mousemanager.Instance.OnMouseClicked += MoveToTarget;
+        Mousemanager.Instance.OnEnemyClicked += EventAttack;
+        GameManager.Instance.RigisterPlayer(characterStats);
+    }
+    void Start()
+    {
+
+        SaveManager.Instance.LoadPlayerData();
+    }
+    private void OnDisable()
+    {
+        if (!Mousemanager.IsInitialized) return;
+        Mousemanager.Instance.OnMouseClicked -= MoveToTarget;
+        Mousemanager.Instance.OnEnemyClicked -= EventAttack;
+    }
+
+
+    private void Update()
+    {
+        isDead = characterStats.CurrentHealth == 0;
+        if (isDead)
+            GameManager.Instance.NotifyObservers();
+        SwitchAnimation();
+        lastAttackTime -= Time.deltaTime;
+    }
+
+    private void SwitchAnimation()
+    {
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
+        anim.SetBool("Death", isDead);
+    }
+    // Update is called once per frame
+    public void MoveToTarget(Vector3 target)
+    {
+        StopAllCoroutines();
+        if (isDead) return;
+        agent.stoppingDistance = stopDistance;
+        agent.isStopped = false;
+        agent.destination = target;
+    }
+    private void EventAttack(GameObject target)
+    {
+        if (isDead) return;
+        if (target != null)
+        {
+            attackTarget = target;
+            characterStats.isCritical = UnityEngine.Random.value < characterStats.attackData.criticalChance;
+            StartCoroutine(MoveToAttackTarget());
+        }
+    }
+    IEnumerator MoveToAttackTarget()
+    {
+        agent.isStopped = false;
+        agent.stoppingDistance = characterStats.attackData.attackRange;
+        transform.LookAt(attackTarget.transform);
+
+        //TODO:修改攻击范围参数
+        while (Vector3.Distance(attackTarget.transform.position, transform.position) > characterStats.attackData.attackRange)
+        {
+            agent.destination = attackTarget.transform.position;
+            yield return null;
+        }
+        agent.isStopped = true;
+        //Attack
+        if (lastAttackTime < 0)
+        {
+            anim.SetBool("Critical", characterStats.isCritical);
+            anim.SetTrigger("Attack");
+            //重置冷却时间
+            lastAttackTime = characterStats.attackData.coolDown;
+        }
+    }
+    //Animation Event
+    void Hit()
+    {
+        if (attackTarget.CompareTag("Attackable"))
+        {
+            if (attackTarget.GetComponent<Rock>()&& attackTarget.GetComponent<Rock>().rockStates == Rock.RockStates.HitNothing)
+            {
+                attackTarget.GetComponent<Rock>().rockStates = Rock.RockStates.HitEnemy;
+                attackTarget.GetComponent<Rigidbody>().velocity = Vector3.one;
+                attackTarget.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.Impulse);
+            }       
+        }
+        else
+        {
+            var targetStats = attackTarget.GetComponent<CharacterStats>();
+            targetStats.TakeDamage(characterStats, targetStats);
+        }
+        
+    }
+}
